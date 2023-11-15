2b14d94a4c3c40f465063781b42f3e2d3ad6caf3
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-11-15 19:25:51 -0300

    Feat: Creating vector embedding

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const apiKey = process.env.OPEN_AI_KEY;
+const apiKey = process.env.OPENAI_API_KEY;
 
 if (!apiKey) {
   throw Error("OPENAI_API_KEY is not set"); //use cleanenv or somithing like that maybe?
@@ -9,3 +9,16 @@ if (!apiKey) {
 const openai = new OpenAI({ apiKey });
 
 export default openai;
+
+export async function getEmbedding(text: string) {
+  const response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: text,
+  });
+
+  const embedding = response.data[0].embedding;
+
+  if (!embedding) throw Error("Error generating embedding.");
+
+  return embedding;
+}
