77b22950f1c9ec413b55467fe3ea2c3870bbcbbb
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-11-15 15:42:40 -0300

    Feat: Creating notes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,17 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "AiNotes - Notes",
 };
 
-export default function NotesPage() {
-  return <div>Here will be your notes</div>;
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("UserId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return <div>{JSON.stringify(allNotes)}</div>;
 }
